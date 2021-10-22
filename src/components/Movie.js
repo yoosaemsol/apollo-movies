b78f2438fe9cddd504b3b6e58b84d6a9bd3013f9
yoosaemsol <yoosaemsol@gmail.com>
2021-10-22 13:24:53 +0900

    Apollo Cache and Styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,29 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-const Movie = ({ id }) => {
+const Container = styled.div`
+  height: 380px;
+  width: 100%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  overflow: hidden;
+  border-radius: 7px;
+`;
+
+const Poster = styled.div`
+  background-image: url(${(props) => props.bg});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+`;
+
+const Movie = ({ id, bg }) => {
   return (
-    <Link to={`/${id}`}>
-      <div>{id}</div>
-    </Link>
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={bg} />
+      </Link>
+    </Container>
   );
 };
 
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,52 @@
 import { useParams } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
+import styled from 'styled-components';
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     movie(id: $id) {
-      id
       title
       medium_cover_image
+      language
+      rating
       description_intro
     }
   }
 `; // first line is for Apollo and then else for GraphQL
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+`;
 
 const Detail = () => {
   const { id } = useParams();
@@ -20,18 +56,23 @@ const Detail = () => {
   });
 
   if (loading) {
-    return 'loading';
+    return <Container></Container>;
   }
 
-  if (data && data.movie) {
-    return (
-      <>
-        <h1>{data.movie.title}</h1>
-        <br />
-        <p>{data.movie.description_intro}</p>
-      </>
-    );
-  }
+  const { title, language, rating, description_intro } = data?.movie;
+
+  return (
+    <Container>
+      <Column>
+        <Title>{title}</Title>
+        <Subtitle>{`${
+          language.charAt(0).toUpperCase() + language.slice(1)
+        } · ${rating}`}</Subtitle>
+        <Description>{description_intro}</Description>
+      </Column>
+      <Poster></Poster>
+    </Container>
+  );
 };
 
 export default Detail;
